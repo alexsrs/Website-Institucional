8665ce33a59594178b4e939996d6bf8f7cf6b23b
Alex Sandro <alex@creapix.com.br>
2019-08-09 15:02:00 -0300

    Slider responsivo
    Criado a animação do slide responsivo na parte de equipe

--- js/sliderEquipe.js
+++ js/sliderEquipe.js
@@ -1,15 +1,49 @@
 $(function(){
 
+	var delay = 3000;
+	var curIndex = 0;
+	var amt;
+
 	initSlider();
+	autoPlay();
 
 	function initSlider(){
 
-		var amt = $('.side-right-wrapper').length;
+		amt = $('.sobre-autor').length;
 		var sizeContainer = 100 * amt;
 		var sizeBoxSingle = 100 / amt;
-		$('side-right-wrapper').css('width',sizeBoxSingle+'%');
-		$('.scrollEquipe-wrapper').css('width',sizeContainer+'%');
+		$('.sobre-autor').css('width',sizeBoxSingle+'%');
+		$('.scroll-wrapper').css('width',sizeContainer+'%');
+
+		for (var i = 0; i < amt; i++) {
+			if(i == 0)
+				$('.slider-bullets').append('<span style="background-color:#4B4949;"></span>');
+			else
+				$('.slider-bullets').append('<span></span>');
+		}
 	}
 
-	
-})
+	function autoPlay(){
+		setInterval(function(){
+			curIndex++;
+			if(curIndex == amt)
+				curIndex = 0;
+			goToSlider(curIndex);
+			
+		},delay)
+	}
+
+	function goToSlider(curIndex){
+		var offSetX = $('.sobre-autor').eq(curIndex).offset().left - $('.scroll-wrapper').offset().left;
+		$('.slider-bullets span').css('background-color','rgb(200,200,200)');
+		$('.slider-bullets span').eq(curIndex).css('background-color','#4B4949');
+		$('.scrollEquipe').stop().animate({'scrollLeft':offSetX});
+
+	}
+
+	$(window).resize(function(){
+		$('.scrollEquipe').stop().animate({'scrollLeft':0});
+	})
+
+
+});
